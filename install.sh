08b2878fac94070372624f9ee2e049a8fc345aeb
Webview <139544238+webappsite@users.noreply.github.com>
2023-08-19 11:08:50 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+apt update && apt upgrade -y
+pkg install tty-clock
+mv fire time-s. time-24 time. time-s hacker /data/data/com.termux/files/usr/bin/
+chmod +x /data/data/com.termux/files/usr/bin/fire
+chmod +x /data/data/com.termux/files/usr/bin/time-s.
+chmod +x /data/data/com.termux/files/usr/bin/time-24
+chmod +x /data/data/com.termux/files/usr/bin/time-s
+chmod +x /data/data/com.termux/files/usr/bin/time.
+chmod +x /data/data/com.termux/files/usr/bin/hacker
+rm -rf $HOME/Bash
+clear
+cd
+echo -e "\e[34mMASTER Install SUCCESSFULLY....[\e[92m✓\e[34m]\e[92m"
