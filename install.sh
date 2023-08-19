8e9eae7a038d878e878f68be81fb0be0f9524612
Webview <139544238+webappsite@users.noreply.github.com>
2023-08-19 11:11:44 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ chmod +x /data/data/com.termux/files/usr/bin/time-24
 chmod +x /data/data/com.termux/files/usr/bin/time-s
 chmod +x /data/data/com.termux/files/usr/bin/time.
 chmod +x /data/data/com.termux/files/usr/bin/hacker
-rm -rf $HOME/Bash
+rm -rf $HOME/Termux_time
 clear
 cd
 echo -e "\e[34mMASTER Install SUCCESSFULLY....[\e[92m✓\e[34m]\e[92m"
